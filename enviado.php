bd7062c108a0b146806f102fb859f199baf9524b
cyberwichi <cyberwichi@gmail.com>
2019-03-06 15:47:45 +0100

    ficheros de respuesta enviado y noenviado .php

---
+++ enviado.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inicio.- Solo San Fernando</title>
+    <link rel="stylesheet" href="estilos/estilos.css">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+
+<body>
+    <div class="row cabeza teal-text text-darken-1 ">
+        <img class="col m1 s5 responsive-img logo" src="./img/logo.png" alt="">
+        <span class="col m10 s5 center cabecera "> SOLO <br> SAN FERNANDO</span>
+
+
+    </div>
+    <div class="container section">
+        <h3 class="center">FORMULARIO CREADO Y ENVIADO CON EXITO</h3>
+        <br>
+        <div class="section">
+        <button onclick="<?php  Header( "Location: http://www.solosanfernando.es/git status");?>"></button>
+        </div>
+
+    </div>
+
+
+
+</body> 
