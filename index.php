028adfb9d1104ec367642cec2418d45789cfb2ca
cyberwichi <cyberwichi@gmail.com>
2019-03-06 01:43:06 +0100

    primer commit

---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inicio.- Solo San Fernando</title>
+    <link rel="stylesheet" href="estilos/estilos.css">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+
+<body>
+    <div class="fixed-action-btn">
+
+        <a href="#" data-target="slide-out" class="btn btn-large teal darken-1 sidenav-trigger"><i class="material-icons">menu</i></a>
+    </div>
+
+
+    <ul id="slide-out" class="sidenav teal darken-1 white-text">
+        <li>
+            <div class="user-view">
+                <div class="background">
+                    <img src="img/fondo.jpg">
+                </div>
+                
+                <a class="btn btn-large teal darken-1 tooltipped unete" data-position="bottom" data-tooltip="Pincha aqui si quieres unirte a nosotros" href="http://www.barbarrero.es/solo">
+                    Unete
+                </a>
+                <br>
+                <a href="#user"><img class="" src="img/logo.png"></a>                
+                <br>
+            </div>
+        </li>
+
+
+        <li><a class="waves-effect teal darken-1 white-text">¿Que pensamos sobre....?</a></li>
+        <li><a class="waves-effect teal darken-1 white-text">¿Quienes somos?</a></li>
+        <li><a class="waves-effect teal darken-1 white-text" href="#!">¿Hacia donde vamos?</a></li>
+        <li><a class="waves-effect teal darken-1 white-text" href="#!">Notas de prensa</a></li>
+        <li><a class="waves-effect teal darken-1 white-text" href="#!">Noticias/Actualidad</a></li>
+        <li><a class="waves-effect teal darken-1 white-text" href="#!">Vamos a charlar a la plazoleta</a></li>
+
+        <li>
+            <div class="divider"></div>
+        </li>
+    </ul>
+
+
+
+    <div class="row cabeza teal darken-1 white-text">
+        <img class="col m1 s5 responsive-img logo" src="./img/logo.png" alt="">
+        <span class="col m10 s5 center cabecera "> SOLO <br> SAN FERNANDO</span>
+       
+
+    </div>
+    <div class="section cuerpo">
+
+
+
+
+        <div class="container section secciones">
+
+
+            <div class="container section">
+                Es un partido politico local dedicado al servicio de los intereses generales
+                de la ciudadanía de San Fernando.
+
+            </div>
+        </div>
+        <div class="parallax-container">
+            <div class="parallax "><img src="img/fondo.jpg">
+            </div>
+
+        </div>
+
+
+
+        <div class="secciones">
+            <div class="container section">
+                <h3>Nuestros fines:</h3>
+                <ul>
+                    <li>Fomentar la transparencia política e institucional en el Ayuntamiento de San Fernando.</li>
+                    <li>Promover la cultura democrática y la participación ciudadana en los asuntos públicos.</li>
+                    <li>Fomentar la gestión pública de los servicios municipales</li>
+                </ul>
+
+            </div>
+        </div>
+
+        <div class="parallax-container">
+            <div class="parallax "><img src="img/alameda.jpeg">
+            </div>
+
+        </div>
+
+        <div class="secciones">
+            <div class="container section">
+                <ul>
+                    <li>La lucha contra la corrupción con todos los medios políticos y legales posibles.</li>
+
+                    <li> Promover el desarrollo del sector primario de San Fernando a través de la defensa y explotación sostenible de su Parque Natural.</li>
+                </ul>
+
+            </div>
+        </div>
+        <div class="parallax-container">
+            <div class="parallax responsive-img"><img src="img/croquer.JPG">
+            </div>
+
+        </div>
+        <div class="secciones">
+            <div class="container section">
+                <ul>
+                    <li> Promover la democracia, tanto la interna dentro del seno del partido, como la externa en las instituciones del Estado</li>
+                </ul>
+            </div>
+
+
+        </div>
+    </div>
+    <div class="parallax-container">
+        <div class="parallax responsive-img"><img src="img/observatorio.jpg">
+        </div>
+
+    </div>
+    <div class="secciones">
+        <div class="container section">
+            <ul>
+                <li> Promover el desarrollo del sector turístico explotando el patrimonio histórico-artístico de la ciudad e investigando sobre el mismo.</li>
+            </ul>
+
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+
+
+
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="script/script.js"></script>
+
+</body> 
